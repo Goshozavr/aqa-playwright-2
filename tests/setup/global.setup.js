92024af57ac998cbc1790464cdacbd7cb25998ad
Bohdan <bohdannikitiuk@gmail.com>
2024-02-02 01:19:15 +0200

    sticky setup

--- tests/setup/global.setup.js
+++ tests/setup/global.setup.js
@@ -1,15 +1,18 @@
 import { test as setup, expect } from "@playwright/test";
-import { STORAGE_STATE } from "../playwright.config";
+import { STORAGE_STATE_USER_PATH } from "../src/data/constants/storageStates";
+import WelcomePage from "../src/pageObjects/welcomePage/WelcomePage";
 
 setup("do login", async ({ page }) => {
   console.log("PROJECT SETUP");
-  await page.goto("/");
-  await page.getByLabel("User Name").fill("user");
-  await page.getByLabel("Password").fill("password");
-  await page.getByText("Sign in").click();
+  const welcomePage = new WelcomePage(page);
+  await welcomePage.visit();
+  await welcomePage.clickSignInButtonAndOpenPopup();
+  await page.getByLabel("Email").fill("aqa-nbm2@test.com");
+  await page.getByLabel("Password").fill("Bbbbbbb7");
+  await page.getByText("Login").click();
 
   // Wait until the page actually signs in.
-  await expect(page.getByText("Hello, user!")).toBeVisible();
+  //await expect(page.getByText("Hello, user!")).toBeVisible();
 
-  await page.context().storageState({ path: STORAGE_STATE });
+  await page.context().storageState({ path: STORAGE_STATE_USER_PATH });
 });
